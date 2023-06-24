cd15ca3e9e74a635c76093b962fcc92ef360a0d5
Bharath <bharathraj7302@gmail.com>
2023-06-24 15:55:57 +0530

    Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide />
+    </div>
+  );
 };
 
 export default Home;
