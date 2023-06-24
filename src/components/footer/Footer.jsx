504268cabb1c2bc44fe9c9853bbbe82791ec4f86
Bharath <bharathraj7302@gmail.com>
2023-06-24 13:02:33 +0530

    Navbar

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.scss"
+
+const Footer = () => {
+  return (
+    <div>
+          <div className="footer">
+              Footer
+      </div>
+    </div>
+  )
+}
+
+export default Footer
