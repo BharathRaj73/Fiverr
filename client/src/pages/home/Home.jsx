32b41fe9dee5bb93ba659ec344268a1d33d1f23c
Bharath <bharathraj7302@gmail.com>
2023-07-02 20:00:00 +0530

    updates

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,9 +4,8 @@ import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 // import Slide from "../../components/slide/Slide";
 import CatCard from "../../components/catCard/CatCard";
-import ProjectCard from "../../components/projectCard/ProjectCard";
 import { cards, projects } from "../../data";
-import { Swiper, SwiperSlide } from "swiper/react";
+
 
 const Home = () => {
   window.scrollTo(0, 0);
